45f9c66a84341ff3e66e6d9cb393ee1cdf9f05e3
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 12:57:51 +0200

    rework workflow

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [[ ${1} == "checkdigests" ]]; then
     mkdir ~/.docker && echo '{"experimental": "enabled"}' > ~/.docker/config.json
@@ -9,10 +10,14 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "Listing packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    echo "Show version info..."
+    docker run --rm --entrypoint="" "${2}" apprise --version
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{APP_VERSION=[^}]*}/{APP_VERSION=${version}}/g" .github/workflows/build.yml
-    version="${version}"
+    echo "VERSION=${version}" > VERSION
     echo "##[set-output name=version;]${version}"
 fi
