26b9d749a434686e9dfb0603241ab4bd945c5a40
mrhotio <26902309+mrhotio@users.noreply.github.com>
2020-12-25 13:00:07 +0100

    skip ci

--- update.sh
+++ update.sh
@@ -18,5 +18,4 @@ else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/apprise/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
     echo '{"version":"'"${version}"'"}' | jq . > VERSION.json
-    echo "##[set-output name=version;]${version}"
 fi
